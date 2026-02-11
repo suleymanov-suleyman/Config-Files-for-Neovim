9fece3f753e6f2673261fe935bf5a6d2edcb7f4d
Suleyman Suleymanov <121731277+suleymanov-suleyman@users.noreply.github.com>
2026-02-11 21:22:30 +0300

    Update indentation settings to use 4 spaces

--- nvim/lua/core/defaults.lua
+++ nvim/lua/core/defaults.lua
@@ -10,8 +10,8 @@ vim.opt.sidescrolloff = 8                          -- Keep 8 columns left/right 
 
 -- Indentation
 vim.opt.tabstop = 4                               -- Tab width
-vim.opt.shiftwidth = 2                             -- Indent width
-vim.opt.softtabstop = 2                            -- Soft tab stop
+vim.opt.shiftwidth = 4                             -- Indent width
+vim.opt.softtabstop = 4                            -- Soft tab stop
 vim.opt.expandtab = true                           -- Use spaces instead of tabs
 vim.opt.smartindent = true                         -- Smart auto-indenting
 vim.opt.autoindent = true                          -- Copy indent from current line
